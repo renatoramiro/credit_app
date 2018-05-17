6c0b84c3752fec3bee5ada176f88e897e2b7b524
Renato Ramiro <renato.ramiro@virtus.ufcg.edu.br>
2018-05-17 15:15:28 -0300

    Added flow when user is not activated or client was not created

--- lib/credit_app_web/controllers/session_controller.ex
+++ lib/credit_app_web/controllers/session_controller.ex
@@ -7,11 +7,16 @@ defmodule CreditAppWeb.SessionController do
   alias CreditApp.{User, Repo}
 
   def create(conn, %{"session" => %{"identity_document" => identity_document, "password" => password}}) do
-    user = Repo.get_by(User, identity_document: identity_document)
+    user = Repo.get_by(User, identity_document: identity_document) |> Repo.preload(:client)
     result = cond do
-      user && checkpw(password, user.password_hash) ->
+      user && checkpw(password, user.password_hash) && user.enabled == true && user.client != nil ->
         new_conn = login(conn, user)
         {:ok, new_conn, get_token(new_conn), get_expiration(new_conn)}
+      user && user.enabled == false ->
+        {:not_activated, conn}
+      user && user.client == nil ->
+        new_conn = login(conn, user)
+        {:client_not_created, new_conn, get_token(new_conn), get_expiration(new_conn)}
       user ->
         {:error, :unauthorized, conn}
       true ->
@@ -24,6 +29,18 @@ defmodule CreditAppWeb.SessionController do
         |> put_resp_header("Authorization", "Bearer #{jwt}")
         |> put_resp_header("x-expires", to_string(exp_token))
         |> render("login.json", user: Repo.preload(user, [:client]))
+      {:not_activated, conn} ->
+        conn
+        |> put_status(412)
+        |> send_resp(:precondition_failed, "{\"message\": \"User was not activated.\", \"id\":#{user.id}, \"condition\": \"not_activated\"}")
+        |> halt()
+      {:client_not_created, new_conn, jwt, exp_token} ->
+        new_conn
+        |> put_status(412)
+        |> put_resp_header("Authorization", "Bearer #{jwt}")
+        |> put_resp_header("x-expires", to_string(exp_token))
+        |> send_resp(:precondition_failed, "{\"message\": \"Client was not created.\", \"id\":#{user.id}, \"condition\": \"client_not_created\"}")
+        |> halt()
       {:error, reason, conn} ->
         conn
         |> send_resp(reason, "{\"message\": \"User was not found with these credentials.\"}")
